fbce8b3d6fde0caa5b97e873a77fc652f80ef1c9
Martinlacorniche <martinvitte@gmail.com>
2025-06-11 17:07:51 +0200

    6

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
