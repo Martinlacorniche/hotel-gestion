d0922ad287e3828f9d649508899ac07016ee9026
Martinlacorniche <martinvitte@gmail.com>
2025-06-12 13:05:07 +0200

    🐛 Fix useSearchParams error with client direct

--- app/update-password/page.tsx
+++ app/update-password/page.tsx
@@ -1,9 +1,10 @@
 import dynamic from "next/dynamic";
 
-// Empêche Next.js de prérendre la page
 export const dynamic = "force-dynamic";
 
-const ClientPage = dynamic(() => import("./client-page"), { ssr: false });
+const ClientPage = dynamic(() => import("./client-page").then(mod => mod.default), {
+  ssr: false,
+});
 
 export default function Page() {
   return <ClientPage />;
